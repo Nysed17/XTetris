7cc8770dbc1bc02ab1978743128e03be2504c530
Alex <95614172+Nysed17@users.noreply.github.com>
2022-03-30 18:24:17 +0200

    tetramini

---
+++ tetramini.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h> 
+#include <string.h> 
+#include <stdlib.h>
+#include <termios.h>
+
+typedef struct grandezze{
+    char tet[100];
+    int size;
+} gr;
+
+struct grandezze g[5];
+
+void tet(){
+    g[1].size = 3;
+    g[1].tet = "#";
+    //tutta sbura niente sostanza
+}
